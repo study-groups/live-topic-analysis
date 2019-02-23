b31cd5906bfa5f238e8765cc0a52d7f00325e9b7
Greg <conditg@gmail.com>
2019-02-23 15:12:22 -0600

    adds init scripting and config

---
+++ start-sparkstream-container.sh
@@ -0,0 +1,2 @@
+
+winpty docker run -it --rm --name pyspark1 -v $spth:/home/jovyan --network thinkful-net jupyter/pyspark-notebook //bin/bash
---
+++ start-tweetgen-container.sh
@@ -0,0 +1,3 @@
+winpty docker run -it --rm --name data_server -v $spth:/home/ds/data --network thinkful-net thinkfulstudent/simple_server //bin/bash
+
+
