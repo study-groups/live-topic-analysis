072bb7495a9b6c6651f9174633e876e7f6cabf7f
Zach Overhulser <zoverhulser@gmail.com>
2021-05-06 18:29:06 -0500

    Uses try catch to handle tests.

--- investigations/sparkline/test-createRingBuffer.js
+++ investigations/sparkline/test-createRingBuffer.js
@@ -4,58 +4,66 @@
     3. .load testFile
 */
 
-function testSize(size) {
-    const rb = createRingBuffer(size);
-    const condition = !assert.equal(rb.buffer.length, size);
+const TESTS = [];
 
-    if (condition) {
-        console.log("Test passes: Size is as expected");
-    } 
+function runTest(fn) {
+    try {
+        fn();
+        console.log("Test passes: ", fn.name);
+    } catch (exception) {
+        console.error(exception);
+    }
 }
-testSize(4);
 
-function testPush(size) {
+TESTS.push(
+    sizeOfBufferIsAsExpected,
+    pushesItemsToTheNextIndex,
+    addsItemsToTheNextIndex,
+    addWillOverWrite,
+    pushesLastItemOut,
+    replacesLastItemInBuffer,
+    getsTheCorrectItemByIndex,
+    getsNextItemInRingBuffer
+);
+TESTS.forEach(runTest);
+
+function sizeOfBufferIsAsExpected() {
+    const size = 4;
+    const rb = createRingBuffer(size);
+    assert.equal(rb.buffer.length, size);
+}
+
+function pushesItemsToTheNextIndex() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.push({id: 1});
     rb.push({id: 2});
-    const condition = !assert.deepEqual(rb.buffer[0], {id: 2}) &&
-        !assert.deepEqual(rb.buffer[1], {id: 1});
-
-    if (condition) {
-        console.log("Test passes: Push method correctly pushes items forward");
-    }
+    assert.deepEqual(rb.buffer[0], {id: 2})
+    assert.deepEqual(rb.buffer[1], {id: 1});
 }
-testPush(4);
 
-function testAdd(size) {
+function addsItemsToTheNextIndex() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.add({id: 1});
     rb.add({id: 2});
-    const condition = !assert.deepEqual(rb.buffer[0], {id: 1}) &&
-        !assert.deepEqual(rb.buffer[1], {id: 2});
-    
-    if (condition) {
-        console.log("Test passes: Add method correctly adds items one after another.");
-    }
+    assert.deepEqual(rb.buffer[0], {id: 1});
+    assert.deepEqual(rb.buffer[1], {id: 2});
 }
-testAdd(4);
 
-function testAddOverWrite(size) {
+function addWillOverWrite() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.add({id: 1});
     rb.add({id: 2});
     rb.add({id: 3});
     rb.add({id: 4});
     rb.add({id: 5});
-    const condition = !assert.deepEqual(rb.buffer[0], {id: 5});
-
-    if (condition) {
-        console.log("Test passes: Add method overwrites when buffer is full.");
-    }
+    assert.deepEqual(rb.buffer[0], {id: 5});
 }
-testAddOverWrite(4);
 
-function testPushLastItemOut(size) {
+function pushesLastItemOut() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.push({id: 1});
     rb.push({id: 2});
@@ -63,69 +71,40 @@ function testPushLastItemOut(size) {
     rb.push({id: 4});
     rb.push({id: 5});
     const lastItem = rb.buffer.length - 1;
-    const condition = !assert.deepEqual(rb.buffer[lastItem], {id: 2});
-
-    if (condition) {
-        console.log("Test passes: Push method pushes last item out.");
-    }
+    assert.deepEqual(rb.buffer[lastItem], {id: 2});
 }
-testPushLastItemOut(4);
 
-function testReplace(size) {
+function replacesLastItemInBuffer() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.add({id: 1});
     rb.add({id: 2})
     rb.replace({id: 3});
-    const condition = !assert.deepEqual(rb.buffer[0], {id: 1}) 
-        && !assert.deepEqual(rb.buffer[1], {id: 3});
-
-    if (condition) {
-        console.log("Test passes: Replace method replaces most recent item")
-    }
+    assert.deepEqual(rb.buffer[0], {id: 1}) 
+    assert.deepEqual(rb.buffer[1], {id: 3});
 }
-testReplace(4);
 
-function testGet(size) {
+function getsTheCorrectItemByIndex() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.add({id: 1});
     rb.add({id: 2});
     rb.add({id: 3});
     rb.add({id: 4});
-    const condition = !assert.deepEqual(rb.get(2), rb.buffer[2]);
-
-    if (condition) {
-        console.log("Test passes: Get method returns correct item by index.");
-    }
+    assert.deepEqual(rb.get(2), rb.buffer[2]);
 }
-testGet(4);
 
-function testGetNext(size) {
+function getsNextItemInRingBuffer() {
+    const size = 4;
     const rb = createRingBuffer(size);
     rb.add({id: 1});
     rb.add({id: 2});
     rb.add({id: 3});
     rb.add({id: 4});
     const { deepEqual } = assert;
-    const condition = !deepEqual(
-        rb.getNext(),
-        rb.buffer[0]
-    )  && !deepEqual(
-        rb.getNext(),
-        rb.buffer[1]
-    ) && !deepEqual(
-        rb.getNext(),
-        rb.buffer[2]
-    ) && !deepEqual(
-        rb.getNext(),
-        rb.buffer[3]
-    ) && !deepEqual(
-        rb.getNext(),
-        rb.buffer[0]
-    );
-
-    if (condition) {
-        console.log("Test passes: GetNext method correctly gets the next item around the circular buffer.")
-    }
-    
+    deepEqual(rb.getNext(), rb.buffer[0]);
+    deepEqual(rb.getNext(), rb.buffer[1]);
+    deepEqual(rb.getNext(), rb.buffer[2]); 
+    deepEqual(rb.getNext(), rb.buffer[3]); 
+    deepEqual(rb.getNext(), rb.buffer[0]);
 }
-testGetNext(4);
