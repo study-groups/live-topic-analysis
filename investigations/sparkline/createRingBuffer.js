d06f6d636451b73b63eefca78023819d58a4aa1e
Mike Ricos <mike.ricos@gmail.com>
2021-05-08 19:33:46 -0500

    New constructor. Works with testsRingBuffer.

--- investigations/sparkline/createRingBuffer.js
+++ investigations/sparkline/createRingBuffer.js
@@ -1,50 +1,60 @@
-/* 
-  createRingBuffer v001
-
-  Ring buffer is a Queue that wraps around and provides
-  get,set operations with modulo indexing.
-
-  Enqueue/Add to back, Dequeue/Get from front.
-  Think of people in line for movie.
-  First in line gets serviced first.
+/*jslint
+    node
 */
-function createRingBuffer(size) {
-    const buffer = new Array(size);
-    const limit = size;
+
+function createRingBuffer(x=0, inplace=false) {
+     let buffer;
+     if(typeof(x)==="number"){
+        buffer = new Array(x);
+    }
+
+    if(Array.isArray(x) && inplace === true){
+         console.log("USING INPLACE, NO COPY")
+         buffer = x;  // dangerously use reference 
+    }
+
+    if(Array.isArray(x) && inplace === false){
+         buffer = [... x]; // make a copy
+    }
+
+    const limit = buffer.length;
     let addIndex = 0;
     let getIndex = 0;
 
+    function pushItems(i = limit - 1) {
+        if (i > 0) {
+            buffer[i] = buffer[i - 1];
+            i = i - 1;
+            return pushItems(i);
+        }
+    }
+
     return {
-        buffer: Object.freeze(buffer),
+        buffer,
+        hello: "hello",
         addIndex,
         getIndex,
 
-        add: function(item) { // back of line == buffer.length-1
-            buffer[addIndex] = {...item}; // make copy
+        add: function (item) { // back of line == buffer.length - 1
+            buffer[addIndex] = item;
             addIndex = (addIndex + 1) % limit;
-            return this;
         },
 
-        replace: function(item) {        // back of line == buffer.length-1
-            buffer[addIndex] = { ...item }; // make copy
-            return this;
+        replace: function (item) {   // back of line == buffer.length-1
+            buffer[addIndex - 1] = item;
         },
 
-        get: (key) => buffer[ key ],     // get2(key){return buffer[key];},
+        get: (key) => buffer[key],
 
-        getNext: function(offset = 0) {
-            retIndex = getIndex;
+        getNext: function (offset = 0) {
+            let retIndex = getIndex;
             getIndex = (buffer.length + getIndex + offset + 1) % limit;
-            return buffer[ retIndex ];
+            return buffer[retIndex];
         },
 
-        push: function(item) { 
-            let i;
-            for(i = limit - 1; i > 0; i = i - 1) {   // b[N-1] = b[N-2]
-                buffer[i] = { ...buffer[i - 1] };            // b[1] = b[0]
-            }
-            buffer[0] = { ...item };
-            return this;
+        push: function (item) {
+            pushItems();
+            buffer[0] = item;
         }
     };
 }
