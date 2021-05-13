fcb92ca5a2d800ba38aa0f48363df3109c44fb2e
Zach Overhulser <zoverhulser@gmail.com>
2021-05-13 15:38:20 -0500

    Initializes model and debugs.

---
+++ investigations/sparkline/app.js
@@ -0,0 +1,21 @@
+// model assumes this function exists and will call it after setModel()
+function updateView() {
+    ReactDOM.render(<App />, document.getElementById("root"));
+}
+
+window.addEventListener("load", function(event) {
+    // localStorage.clear();
+    // if local storage does NOT contain a key named "model"
+    // create the key of "model" and pass its initial state
+    if (localStorage.getItem("model") === null) {
+        setObject("model",
+            { 
+                isStreamOn: false,
+                data: [... Array(10)],
+                data2: RingBuffer([1,2,3,4,5,6,7,8,9,0]).toJson()
+            }
+        );
+        console.log("localStorage Model initialized.");
+    }
+    updateView();
+});
