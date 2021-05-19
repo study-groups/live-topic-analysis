135de47a17ba8b38dcf48690b7ead8e8eb0dfb35
Zach Overhulser <zoverhulser@gmail.com>
2021-05-19 17:42:01 -0500

    Sets up options for line graph and adds line graph data to model.

--- investigations/sparkline/z-controller.js
+++ investigations/sparkline/z-controller.js
@@ -1,30 +1,55 @@
 //CONTROLLERS
 function handleGetData() {
+
+    const container = document.getElementById("sparkline-container");
+
+    const options = {
+        //size: [100, 20],
+        size: [500, 200],
+        value: {
+            x: d => d.date,
+            y: d => d.value
+        }
+    };
         // GET
-        fetch(DATA_SERVER_URL)
-            .then(handleErrors)
-            // convert Buffer to JSON
-            .then(response => response.json())
-            .then(function(jsonObject) {
-                const dataArray = getModel().data; // ring buffer array 
-                const rb = RingBuffer(dataArray);
-                rb.push(jsonObject);
-                //const rb2 = new RingBuffer(Array(10));
-                const rb2 = new RingBuffer([1,2,3,4,5,6,7,8,9,10]);
-                rb2.fromJson(getModel().data2);  // RingBuffer object
-                console.log("Rb2:",rb2);
-                rb2.add(jsonObject);
-                console.log("Rb2 after:",rb2);
-                setModel({
-                    // use previous state
-                    ...getModel(),
-                    // old data plus the new from response
-                    data: [...JSON.parse(rb.toJson()).d],
-                    data2: rb2.toJson()
-                });
-                console.log(getModel().data);
-            })
-            .catch(error => console.log(error))
+    fetch(DATA_SERVER_URL)
+        .then(handleErrors)
+        // convert Buffer to JSON
+        .then(response => response.json())
+        .then(function(jsonObject) {
+            const dataArray = getModel().data; // ring buffer array 
+            const rb = RingBuffer(dataArray);
+            rb.push(jsonObject);
+            //const rb2 = new RingBuffer(Array(10));
+            const rb2 = new RingBuffer([1,2,3,4,5,6,7,8,9,10]);
+            rb2.fromJson(getModel().data2);  // RingBuffer object
+            rb2.add(jsonObject);
+
+            const now = Date.now();
+            const lineGraph = JSON.parse(
+                    rb.toJson()
+            ).d.map(function (value, i) {
+                return {
+                    date: new Date(now - (i * 24 * 60 * 60 * 1000)),
+                    value: value !== null ? value.data : 0
+                };
+            });
+
+            sparkline(container, lineGraph, options);
+
+            console.log("lineGraph: ", lineGraph);
+                            
+            setModel({
+                // use previous state
+                ...getModel(),
+                // old data plus the new from response
+                data: [...JSON.parse(rb.toJson()).d],
+                data2: rb2.toJson(),
+                lineGraph: [...getModel().lineGraph, ...lineGraph] 
+            });
+            console.log(getModel());
+        })
+        .catch(error => console.log(error))
 }
 
 function handleClick() {
