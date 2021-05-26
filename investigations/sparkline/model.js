4179f00eb3f3ca762aa3e9f6327448ccb8bce255
Zach Overhulser <zoverhulser@gmail.com>
2021-05-25 19:45:24 -0500

    Takes job into account.

--- investigations/sparkline/model.js
+++ investigations/sparkline/model.js
@@ -1,32 +1,4 @@
 // MODEL INIT
-
-// Debug
-localStorage.clear();
-
-// if local storage does already contain a key named "model"
-// set the model to the most recent model
-if (localStorage.getItem("model") !== null) {
-    setModel({...getModel()});
-}
-
-// if local storage does NOT contain a key named "model"
-// create the key of "model" and pass its initial state
-if (localStorage.getItem("model") === null) {
-    setObject("model",
-        { 
-            isStreamOn: false,
-            data: [{ id: 1, type: "random.number", data: 121 }],
-        }
-    );
-}
-
-const MOUNT_POINT = document.getElementById("root");
-
-// MODEL
-function updateView(mountPoint = MOUNT_POINT) {
-    ReactDOM.render(<App />, mountPoint);
-}
-
 function getObject(key) {
     // gets value from local storage by key
     // parses string value to object
@@ -49,4 +21,3 @@ function setModel(obj) {
     setObject("model", obj);
     updateView(); // re-renders Virtual DOM tree
 }
-
